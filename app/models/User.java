1937efd807ef0cb6a3b4a5449c771406c793e476
Jenny Olsson <rymdsnigel@gmail.com>
2013-01-28 13:35:21 +0100

    Created model User

---
+++ app/models/User.java
@@ -0,0 +1,52 @@
+package models;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import play.data.format.Formats;
+import play.data.validation.Constraints;
+import play.db.ebean.Model;
+
+@Entity 
+@Table(name="account")
+public class User {
+	
+    @Id
+    @Constraints.Required
+    @Formats.NonEmpty
+    public String username;
+    
+    @Constraints.Required
+    public String email;
+    
+    @Constraints.Required
+    public String surname;
+    
+    @Constraints.Required
+    public String lastname;
+    
+    @Constraints.Required
+    public Integer birthdate;
+    
+    
+    public static Model.Finder<String,User> find = new Model.Finder(String.class, User.class);
+
+    /**
+     * Retrieve a User from email.
+     */
+    public static User findByEmail(String email) {
+        return find.where().eq("email", email).findUnique();
+    }
+    
+    /**
+     * Authenticate a User.
+     */
+    public static User authenticate(String username, String password) {
+        return find.where()
+            .eq("username", username)
+            .eq("password", password)
+            .findUnique();
+    }
+
+}
